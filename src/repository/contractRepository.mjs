fd2ee178093539e5ce17a65e398ab6c3bd03a3ee
Daniel Gorbatov <daniel.gorbatov@gmail.com>
2024-07-30 22:59:08 +0300

    Replace string literals with enums

--- src/repository/contractRepository.mjs
+++ src/repository/contractRepository.mjs
@@ -1,5 +1,6 @@
 import { Op } from 'sequelize';
 import Contract from '../model/contractModel.mjs';
+import { ContractStatuses } from '../config/enums.mjs';
 
 async function findOneByIdAndProfileId(contractId, profileId) {
   const contract = await Contract.findOne({
@@ -18,7 +19,7 @@ async function findByProfileIdAndTerminatedFalse(profileId) {
   const contracts = Contract.findAll({
     where: {
       status: {
-        [Op.ne]: 'terminated',
+        [Op.ne]: ContractStatuses.TERMINATED,
       },
       [Op.or]: [
         { ClientId: profileId },
